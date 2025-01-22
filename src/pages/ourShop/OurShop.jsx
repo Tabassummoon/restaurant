030d21e178ea41e317865906f682dde56c0736f0
unknown <tabassummon8@gmail.com>
2025-01-22 17:29:09 +0600

    new project

--- src/pages/ourShop/OurShop.jsx
+++ src/pages/ourShop/OurShop.jsx
@@ -1,8 +1,65 @@
+import ourShop from "../../assets/shop/banner2.jpg"
+import useMenu from "../../hooks/useMenu";
+import CoverImage from "../shared/CoverImage";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
+import OurShopTab from "./OurShopTab";
 
 const OurShop = () => {
+
+    const [menu] = useMenu()
+    const drink = menu.filter(food=> food.category === "drinks")
+    const dessert = menu.filter(food=> food.category === "dessert")
+    const pizza = menu.filter(food=> food.category === "pizza")
+    const salad = menu.filter(food=> food.category === "salad")
+    const soup = menu.filter(food=> food.category === "soup")
+   console.log(drink,dessert,pizza,salad,soup)
   return (
     <div>
-      <h1>our shop</h1>
+<CoverImage
+title="Our Shop"
+bannerImg={ourShop}
+></CoverImage>
+<div>
+
+
+<Tabs className="max-w-7xl mx-auto">
+    <TabList className="w-96 mx-auto">
+      <Tab>Salad</Tab>
+      <Tab>Pizza</Tab>
+      <Tab>Soup</Tab>
+      <Tab>Dessert</Tab>
+      <Tab>Drinks</Tab>
+    </TabList>
+
+    <TabPanel >
+    <OurShopTab
+    foods={salad}
+    ></OurShopTab>
+    </TabPanel>
+    <TabPanel>
+      <OurShopTab
+      foods={soup}
+      ></OurShopTab>
+    </TabPanel>
+    <TabPanel>
+    <OurShopTab
+      foods={pizza}
+      ></OurShopTab>
+    </TabPanel>
+    <TabPanel>
+    <OurShopTab
+      foods={dessert}
+      ></OurShopTab>
+    </TabPanel>
+    <TabPanel>
+    <OurShopTab
+      foods={drink}
+      ></OurShopTab>
+    </TabPanel>
+  </Tabs>
+  </div>
     </div>
   );
 };
